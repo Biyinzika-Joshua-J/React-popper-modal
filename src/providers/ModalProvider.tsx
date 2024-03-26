e544d4080bb4209311fdca4080785c4a6f6f5c44
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2024-03-26 19:12:43 +0300

    testing husky

---
+++ src/providers/ModalProvider.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ModalProvider = () => {
+const test = () => console.log("test");
+  return (
+    <div>ModalProvider</div>
+  )
+}
+
+export default ModalProvider
